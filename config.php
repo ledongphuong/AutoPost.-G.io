60e2d6e5e048546396a257adc087d5974c7b7562
PhuongLND <phuonglnd@rikkeisoft.com>
2022-12-14 09:13:02 +0700

    fist commit

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ ___    _              _              _    _ 
+| . | _| | ___ ._ _ _ | |__ ___  _ _ | |__<_>
+|   |/ . |/ ._>| ' ' || / // . \| | || / /| |
+|_|_|\___|\___.|_|_|_||_\_\\___/`___||_\_\|_|
+                                             
+ By @AdemKouki
+ GitHub : https://github.com/Ademking/InstaPoster
+ Email : ademkingnew@gmail.com
+ Facebook : https://github.com/Ademking/InstaPoster
+
+ */
+$username = 'wiin_131';
+$password = 'dongphuong1205';
+
+$image_description = 'Your Description Here ...'; 
+$imgurl = 'https://picsum.photos/700/?random';
+$photoFilename = "img/rand.jpg";
